1e721500dc1e6a809eccc06885368b5a545d9750
tansudasli <tansudasli@gmail.com>
2023-07-04 23:18:02 +0300

    removed GCP ai-platform specific codes

--- .get-sample-data.sh
+++
@@ -1,3 +0,0 @@
-# downloads data set
-# run on local
-gsutil -m cp -r  gs://cloud-samples-data/ml-engine/census/data/ .
--- train-job-locally-checklogs.sh
+++
@@ -1,8 +0,0 @@
-# run on gshell
-# to see local training job logs
-
-source .env
-
-tensorboard --logdir=$MODEL_DIR --port=8080
-
-# preview on 8080
