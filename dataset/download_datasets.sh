6b77734fd695157c19700820271837cde980a27e
tansudasli <tansudasli@gmail.com>
2023-08-01 13:06:11 +0300

    dataset file download script

---
+++ dataset/download_datasets.sh
@@ -0,0 +1,10 @@
+curl -O https://storage.googleapis.com/download.tensorflow.org/data/abalone_test.csv
+curl -O https://storage.googleapis.com/download.tensorflow.org/data/abalone_train.csv
+
+mv abalone_test.csv abalone.test
+mv abalone_train.csv abalone.csv
+
+curl -O https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test
+curl -O https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data
+
+
